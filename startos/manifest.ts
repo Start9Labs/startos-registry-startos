9569789c9398985fb8ea6ff1ce3cbfdc176e8411
Matt Hill <mattnine@protonmail.com>
2025-12-12 13:52:45 -0700

    clean up

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,10 +1,4 @@
 import { setupManifest } from '@start9labs/start-sdk'
-import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
-
-const BUILD = process.env.BUILD || ''
-
-const architectures =
-  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'startos-registry',
@@ -25,12 +19,8 @@ export const manifest = setupManifest({
   volumes: ['main'],
   images: {
     'startos-registry': {
-      source: { dockerTag: 'ghcr.io/start9labs/startos-registry' },
-      arch: architectures,
-    } as SDKImageInputSpec,
-  },
-  hardwareRequirements: {
-    arch: architectures,
+      source: { dockerTag: 'ghcr.io/start9labs/startos-registry:next-major' },
+    },
   },
   alerts: {
     install: null,
