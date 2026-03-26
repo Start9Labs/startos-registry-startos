2c78e5ac011ee6cedb649e866ec85b1edee0da45
Matt Hill <mattnine@protonmail.com>
2026-03-26 17:32:20 -0600

    initial beta

--- startos/versions/v0.4.0.0.b0.ts
+++ startos/versions/v0.4.0.0.b0.ts
@@ -1,7 +1,7 @@
-import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
-export const v_0_4_0_0 = VersionInfo.of({
-  version: '0.4.0:0',
+export const v_0_4_0_0_b0 = VersionInfo.of({
+  version: '0.4.0:0-beta.0',
   releaseNotes: {
     en_US: 'Initial release for StartOS 0.4.0',
     es_ES: 'Versión inicial para StartOS 0.4.0',
