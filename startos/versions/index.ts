70caf6366985b347718f27e103b6788adf941526
Helix <helix@start9.com>
2026-05-12 19:02:26 +0000

    chore: bump start-sdk to 1.5.0
    Bumps @start9labs/start-sdk from 1.3.3 to 1.5.0 and rolls the package
    revision to 0.4.0:3. No upstream image change (still tracks
    ghcr.io/start9labs/startos-registry:master).

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_4_0_2 } from './v0.4.0.2'
+import { v_0_4_0_3 } from './v0.4.0.3'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_4_0_2,
+  current: v_0_4_0_3,
   other: [],
 })
